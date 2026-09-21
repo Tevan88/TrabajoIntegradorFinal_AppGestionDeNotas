5d0a348e0a18ccea3ca8a3afb44c8bbda5a4deec
donramones <agusrivarolaa00@gmail.com>
2026-09-21 19:23:03 -0300

    Agregar archivos del proyecto

---
+++ src/main/java/TrabajoIntegradorFinal_AppGestionDeNotas/TrabajoIntegradorFinal_AppGestionDeNotas/TrabajoIntegradorFinalAppGestionDeNotasApplication.java
@@ -0,0 +1,13 @@
+package TrabajoIntegradorFinal_AppGestionDeNotas.TrabajoIntegradorFinal_AppGestionDeNotas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabajoIntegradorFinalAppGestionDeNotasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabajoIntegradorFinalAppGestionDeNotasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/TrabajoIntegradorFinal_AppGestionDeNotas/TrabajoIntegradorFinal_AppGestionDeNotas/TrabajoIntegradorFinalAppGestionDeNotasApplicationTests.java
@@ -0,0 +1,13 @@
+package TrabajoIntegradorFinal_AppGestionDeNotas.TrabajoIntegradorFinal_AppGestionDeNotas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabajoIntegradorFinalAppGestionDeNotasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
